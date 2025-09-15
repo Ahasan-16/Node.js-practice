ed6403647a96963c82e2f46a8dc71f4fd9e8ea0c
Ahasan-16 <mdahasan1916@gmail.com>
2025-09-15 19:54:23 +0600

    rename in fs module  asyncronous way

--- main.js
+++ main.js
@@ -3,17 +3,20 @@ var http = require('http');
 var fs=  require('fs');
 
 var server=http.createServer(function(req,res) {
-
-    //writeFileSync protme file er name, tarpor file e ki content takbe ,r ei ta error return kore
-    var error=fs.writeFileSync('documentaion.txt', 'this is documentation form syncronous');
-        if (error) {
-            res.writeHead(404, {'Content-Type': 'text/plain'});
-            res.write("file write fail");
-            res.end();
-        } else {
-            res.writeHead(200, {'Content-Type': 'text/plain'});
-            res.write("file write success");
+    //asyncronous
+    //rename e protome old path ,tarpor new path, tarpor callback function
+    fs.rename('documentaion.txt','documentation.txt',function(error){
+        if(error){
+            res.writeHead(404,{'Content-Type':'text/plain'});
+            res.write("file rename fail");
             res.end();
         }
+        else
+        {
+            res.writeHead(200,{'Content-Type':'text/plain'});
+            res.write("file rename success");
+            res.end();
+        }
+    })
 })
 server.listen(8080);
