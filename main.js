36ed9a1c227ac87dc879e945c4fd6e2b9ddac7c8
Ahasan-16 <mdahasan1916@gmail.com>
2025-09-15 12:43:48 +0600

    request response using http

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
-var http = require('http');
-
+var http=require('http');
 var server=http.createServer(function(req,res){
-    res.end('server created by Ahasan');
+    //kon request asle ki korbo oi ta handle korbo req parameter er maddome
+    if(req.url=="/")
+    {
+        //response ta patabo head abong body niye res parameter er maddome
+        res.writeHead(200,{'Content-Type':'text/html'});
+        res.write('<h1>This is a home page</h1>');
+        res.end();
+    }
+    else if(req.url=="/about")
+    {
+        res.writeHead(200,{'Content-Type':'text/html'});
+        res.write('<h1>This is a about page</h1>');
+        res.end();
+    }
+    else if(req.url=="/contact")
+    {
+        res.writeHead(200,{'Content-Type':'text/html'});
+        res.write('<h1>This is a contact page</h1>');
+        res.end();
+    }
+
 });
-server.listen(2020);
-console.log("server run successful!");
+server.listen(4040);
+console.log("server run successfully");
