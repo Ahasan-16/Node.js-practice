03fdf6c4536c76304dd0c9a0726161ddccd13a5f
Ahasan-16 <mdahasan1916@gmail.com>
2025-09-15 21:58:05 +0600

    creating website in server

--- main.js
+++ main.js
@@ -3,15 +3,21 @@ var http = require('http');
 var fs=  require('fs');
 
 var server=http.createServer(function(req,res) {
-    //syncronous way for checking file ache ki nai
-    //existsSync e only file path jei ta ache ki na check korbo, ei ta exists kore ki na ei ta return kore
-    var exists=fs.existsSync('index.html');
-        if (exists) {
-            res.end("file exists");
-        }
-        else {
-            res.end("file not exists");
-        }
+    if(req.url === '/'){
+        var data= fs.readFileSync('home.html');
+        res.end(data);
+    }
+    else if(req.url=='/contact')
+    {
+        var data= fs.readFileSync('contact.html');
+        res.end(data);
+    }
+    else if(req.url=='/about')
+    {
+        var data= fs.readFileSync('about.html');
+        res.end(data);
+    }
+
 
 
 })
