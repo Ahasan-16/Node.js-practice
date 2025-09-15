8308c877a3550494cafc719ad9a5dfb2470af791
Ahasan-16 <mdahasan1916@gmail.com>
2025-09-15 20:22:45 +0600

    exists in fsmodule asyncronous way

--- main.js
+++ main.js
@@ -3,21 +3,17 @@ var http = require('http');
 var fs=  require('fs');
 
 var server=http.createServer(function(req,res) {
-
-    //syncronous way file delete
-    //unlinkSync e only file path,ei ta error return kore
-        var error= fs.unlinkSync('new.txt');
-        if(error){
-            res.writeHead(404,{'Content-Type':'text/plain'});
-            res.write("file delete fail");
-            res.end();
+    //asyncronous way for checking file ache ki nai
+    //exists e protome file path jei ta ache ki na check korbo, porerta callback function
+    fs.exists('index.html', function(exists) {
+        if (exists) {
+            res.end("file exists");
         }
         else
         {
-            res.writeHead(200,{'Content-Type':'text/plain'});
-            res.write("file delete success");
-            res.end();
+            res.end("file not exists");
         }
+    })
 
 
 })
